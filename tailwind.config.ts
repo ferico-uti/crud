bb7234633b7d38595973510438a71fe7e24e2895
ferico-uti <fericopasaribu@teknokrat.ac.id>
2024-12-30 19:56:10 +0700

    budi: upload project pertama kali

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,40 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    // colors: {
+    //   'color1' : '#133E87',
+    //   'color2' : '#F3F3E0',
+    // },
+
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        'color1' : '#133E87',
+        'color2' : '#F3F3E0',
+        'color3' : {
+          '1' : '#ff7849',
+          '2' : '#7e5bef',
+          '3' : {
+            'a' : "#FFCC00"
+          }
+        }
       },
+      spacing: {
+        '5X' : '5px',
+        '15X' : '15px',
+        '25X' : '25px',
+        '10%' : '10%',
+        '30%' : '30%',
+
+      }
     },
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: ["light", "dark", "corporate"],
+  },
 };
 export default config;
